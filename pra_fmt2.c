1d85334ff8e5b4f4aa533e77120005e926795bfb
snappyJack <zhangzycn@foxmail.com>
2019-09-18 10:53:25 +0800

    Create pra_fmt2.c

---
+++ pra_fmt2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int what_can_i_do() {
+    return puts("do nothing");
+}
+
+int main(void) {
+    int (*func)();
+    func = &what_can_i_do;
+
+    char buf[1024];
+    printf("get something at %p ", &func);
+    fflush(stdout);
+    fgets(buf, 1024, stdin);
+    printf(buf);
+    (*func)();
+
+    return 0;
+}
